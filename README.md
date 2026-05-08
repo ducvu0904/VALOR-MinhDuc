ceb9f4be18250675082cb8c4a9ca09ac16498607
ducvu0904 <duc.vu0904@gmail.com>
2026-05-08 19:57:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VALOR-MinhDuc
+Reproduce paper VALOR: Value-Aware Revenue Uplift Modeling with Treatment-Gated Representation for B2B Sales
---
+++ models/baselines.py
@@ -0,0 +1,17 @@
+"""Baseline Models:
+     - Tarnet
+     - Dragonnet
+     - CFR (wass/mmd)
+     - UniTE
+     - EUEN
+     - T-learner
+     - S-learner
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np 
+
+class 
+
+
